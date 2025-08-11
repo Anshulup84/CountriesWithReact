f6bde50c7ceae5e9a0eb9ba8a6a1a8e684a21ba8
Anshul <avenger720720@gmail.com>
2025-08-11 18:11:27 +0530

    added country card component

---
+++ components/CountriesContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CountriesContainer() {
+  return (
+   <div className="countries-container">
+    
+   </div>
+  )
+}
---
+++ components/CountryCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function CountryCard() {
+  return (
+   <a class="country-card" href="./country.html?name=Guyana">
+                        <img src="https://flagcdn.com/gy.svg" alt="Guyana Flag"/>
+                        <div class="card-text">
+                            <h3 class="card-title">Guyana</h3>
+                            <p><b>Population: </b>7,86,559</p>
+                            <p><b>Region: </b>Americas</p>
+                            <p><b>Capital: </b>Georgetown</p>
+                        </div>
+                       
+                    </a>
+  )
+}
